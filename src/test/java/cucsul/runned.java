2199cef4c46ca60a5f965e4a1550c13eea04ea73
Training_C2a.04.28 <Training_C2a.04.28@CDC2-D-4BN1N62.dir.svc.accenture.com>
2019-09-06 16:15:20 +0530

    push up

---
+++ src/test/java/cucsul/runned.java
@@ -0,0 +1,13 @@
+package cucsul;
+
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(monochrome=true,plugin= {"pretty","json:sultan.json"})
+
+public class runned {
+
+}
